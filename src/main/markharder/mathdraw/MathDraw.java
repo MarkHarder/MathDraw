b01365be3b2f83ede37f12f27c0571b54e4b96b1
MarkHarder <mark.harder899@gmail.com>
2013-02-09 17:03:07 -0600

    Add circle drawing

--- src/main/markharder/mathdraw/MathDraw.java
+++ src/main/markharder/mathdraw/MathDraw.java
@@ -35,7 +35,7 @@ public class MathDraw extends JComponent {
     private String mode;
 
     public MathDraw() {
-        mode = "Rotate Rectangle";
+        mode = "Circle";
     }
 
     public void paint(Graphics g) {
@@ -52,11 +52,12 @@ public class MathDraw extends JComponent {
             for (double theta = 0.0; theta < 90.0; theta += 15) {
 
                 double radians = theta * Math.PI / 180;
+                int width = 200;
 
-                Point p1 = new Point((int) (200 * Math.sin(radians) + center.getX()), (int) (200 * Math.cos(radians) + center.getY()));
-                Point p2 = new Point((int) (200 * Math.sin(radians + Math.PI / 2) + center.getX()), (int) (200 * Math.cos(radians + Math.PI / 2) + center.getY()));
-                Point p3 = new Point((int) (200 * Math.sin(radians + Math.PI) + center.getX()), (int) (200 * Math.cos(radians + Math.PI) + center.getY()));
-                Point p4 = new Point((int) (200 * Math.sin(radians + 3 * Math.PI / 2) + center.getX()), (int) (200 * Math.cos(radians + 3 * Math.PI / 2) + center.getY()));
+                Point p1 = new Point((int) (width * Math.sin(radians) + center.getX()), (int) (width * Math.cos(radians) + center.getY()));
+                Point p2 = new Point((int) (width * Math.sin(radians + Math.PI / 2) + center.getX()), (int) (width * Math.cos(radians + Math.PI / 2) + center.getY()));
+                Point p3 = new Point((int) (width * Math.sin(radians + Math.PI) + center.getX()), (int) (width * Math.cos(radians + Math.PI) + center.getY()));
+                Point p4 = new Point((int) (width * Math.sin(radians + 3 * Math.PI / 2) + center.getX()), (int) (width * Math.cos(radians + 3 * Math.PI / 2) + center.getY()));
 
                 g2.setColor(Color.BLUE);
                 g2.setStroke(new BasicStroke(4F));
@@ -64,6 +65,18 @@ public class MathDraw extends JComponent {
                 g2.drawLine((int) p2.getX(), (int) p2.getY(), (int) p3.getX(), (int) p3.getY());
                 g2.drawLine((int) p3.getX(), (int) p3.getY(), (int) p4.getX(), (int) p4.getY());
                 g2.drawLine((int) p4.getX(), (int) p4.getY(), (int) p1.getX(), (int) p1.getY());
+            }
+        } else if (mode.equals("Circle")) {
+            for (double theta = 0.0; theta == 0.0 || theta % 360.0 != 0; theta += 30) {
+                double radians = theta * Math.PI / 180;
+                int center_diameter = 150;
+                int diameter = 150;
+
+                Point p1 = new Point((int) (center_diameter * Math.sin(radians) + center.getX()), (int) (center_diameter * Math.cos(radians) + center.getY()));
+
+                g2.setColor(Color.GREEN);
+                g2.setStroke(new BasicStroke(4F));
+                g2.drawOval((int) (p1.getX() - diameter / 2), (int) (p1.getY() - diameter / 2), diameter, diameter);
             }
         }
     }
@@ -85,7 +98,7 @@ public class MathDraw extends JComponent {
 
         frame.setUndecorated(true);
         frame.pack();
-        
+
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
 
